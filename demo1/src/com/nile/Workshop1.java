6f68e0b96ad8f865391668873a7b938e596193ab
Matas Paosriwong <matas.p@kbtg.tech>
2021-06-17 14:59:37 +0700

    add

--- demo1/src/com/nile/Workshop1.java
+++ demo1/src/com/nile/Workshop1.java
@@ -1,10 +1,12 @@
 package com.nile;
 
+import static java.lang.Integer.parseInt;
+
 public class Workshop1 {
     public static void main(String[] args) {
         String input = "(1,5]";
-        int firstNum = Character.getNumericValue(input.charAt(1));
-        int lastNum = Character.getNumericValue(input.charAt(3));
+        int firstNum = parseInt(input.substring(1,2));
+        int lastNum = parseInt(input.substring(3,4));
         firstNum = input.startsWith("[") ? firstNum : firstNum + 1;
         lastNum = input.endsWith("]") ? lastNum + 1 : lastNum;
         for (int i = firstNum; i < lastNum; i++) {
