345b3985afd1618043477532358ac77591eec86c
Matas Paosriwong <matas.p@kbtg.tech>
2021-06-17 15:03:57 +0700

    edit

--- demo1/src/com/nile/Workshop1.java
+++ demo1/src/com/nile/Workshop1.java
@@ -4,12 +4,12 @@ import static java.lang.Integer.parseInt;
 
 public class Workshop1 {
     public static void main(String[] args) {
-        String input = "(1,5]";
+        String input = "[1,5]";
         int firstNum = parseInt(input.substring(1,2));
         int lastNum = parseInt(input.substring(3,4));
         firstNum = input.startsWith("[") ? firstNum : firstNum + 1;
-        lastNum = input.endsWith("]") ? lastNum + 1 : lastNum;
-        for (int i = firstNum; i < lastNum; i++) {
+        lastNum = input.endsWith("]") ? lastNum  : lastNum -1 ;
+        for (int i = firstNum; i <= lastNum; i++) {
             System.out.print(i);
         }
 
