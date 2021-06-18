f725a197fd07f9b97728b87dacbda6491dce1c0d
Matas Paosriwong <matas.p@kbtg.tech>
2021-06-18 14:54:07 +0700

    add test 1 & test 2

--- hello-spring/src/main/java/com/nile/hellospring/workshop02/FizzBuzz.java
+++ hello-spring/src/main/java/com/nile/hellospring/workshop02/FizzBuzz.java
@@ -1,4 +1,7 @@
 package com.nile.hellospring.workshop02;
 
 public class FizzBuzz {
+    public String getResult(int num){
+        return String.valueOf(num);
+    }
 }
--- hello-spring/src/test/java/com/nile/hellospring/workshop02/FizzBuzzTest.java
+++ hello-spring/src/test/java/com/nile/hellospring/workshop02/FizzBuzzTest.java
@@ -1,7 +1,24 @@
 package com.nile.hellospring.workshop02;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+    @Test
+    @DisplayName("Test with number = 1")
+    public void Test1(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.getResult(1);
+        assertEquals("1" , result);
+    }
 
+    @Test
+    @DisplayName("Test with number = 2")
+    public void Test2(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.getResult(2);
+        assertEquals("2" , result);
+    }
 }
