22e5af26c9e686a4719f0bec3bdd6ed66e52d297
Matas Paosriwong <matas.p@kbtg.tech>
2021-06-21 14:03:48 +0700

    start server in test

--- hello-rest/src/main/java/com/nile/hellorest/controller/EmployeeController.java
+++ hello-rest/src/main/java/com/nile/hellorest/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.nile.hellorest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeByID(@PathVariable(name="id") String id){
+        int _id = 0;
+        try{
+            _id = Integer.parseInt(id);
+        }catch (NumberFormatException e){
+            System.out.println("NumberFormatException");
+        }
+        return new EmployeeResponse(_id , "Matas" , "N");
+    }
+
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeByID2(@RequestParam(name = "id" , defaultValue = "1") String id){
         int _id = 0;
         try{
             _id = Integer.parseInt(id);
---
+++ hello-rest/src/test/java/com/nile/hellorest/controller/EmployeeControllerTest.java
@@ -0,0 +1,15 @@
+package com.nile.hellorest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerTest {
+
+    @Test
+    public void callAPIWithPathVariable(){
+
+    }
+}
