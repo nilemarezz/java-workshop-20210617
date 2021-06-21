9dcf9a23694b9cfd878a861138a5f006189e58ab
Matas Paosriwong <matas.p@kbtg.tech>
2021-06-21 14:11:47 +0700

    add test request params

--- hello-rest/src/test/java/com/nile/hellorest/controller/EmployeeControllerTest.java
+++ hello-rest/src/test/java/com/nile/hellorest/controller/EmployeeControllerTest.java
@@ -1,15 +1,31 @@
 package com.nile.hellorest.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerTest {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     public void callAPIWithPathVariable(){
+        EmployeeResponse response = restTemplate.getForObject("/employee/123" , EmployeeResponse.class);
+        assertEquals(123 , response.getId());
+        assertEquals("Matas" , response.getFname());
+        assertEquals("N" , response.getSname());
+    }
 
+    @Test
+    public void callAPIWithRequestParameter(){
+        EmployeeResponse response = restTemplate.getForObject("/employee?id=123" , EmployeeResponse.class);
+        assertEquals(123 , response.getId());
+        assertEquals("Matas" , response.getFname());
+        assertEquals("N" , response.getSname());
     }
 }
